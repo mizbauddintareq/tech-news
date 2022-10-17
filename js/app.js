f28b05441e2d1421c2df1ddff6f5f34fee56a2fa
mizbauddintareqe <mizbauddintareq@gmail.com>
2022-10-17 14:36:53 +0600

    redesign news card

--- js/app.js
+++ js/app.js
@@ -9,7 +9,6 @@ const loadCategories = async () => {
     console.log(error);
   }
 };
-
 const displayCategories = async () => {
   const allCategories = await loadCategories();
   allCategories.forEach((cat) => {
@@ -17,14 +16,13 @@ const displayCategories = async () => {
 
     const li = document.createElement("li");
     li.innerHTML = `
-      <a class="nav-link fw-bold text-dark" onclick="loadNews(${cat.category_id}, '${cat.category_name}')">${cat.category_name}</a>
+      <a class="nav-link" onclick="loadNews(${cat.category_id}, '${cat.category_name}')">${cat.category_name}</a>
       `;
     categoriesContainer.appendChild(li);
   });
 };
 
 const loadNews = async (id, name) => {
-  toggleLoader(true);
   try {
     const res = await fetch(
       `https://openapi.programming-hero.com/api/news/category/0${id}`
@@ -36,11 +34,12 @@ const loadNews = async (id, name) => {
   }
 };
 
-const displayNews = (data, name) => {
-  const sortedByView = data.sort((a, b) => b.total_view - a.total_view);
+const displayNews = (news, name) => {
+  console.log(news);
+  const sortedByView = news.sort((a, b) => b.total_view - a.total_view);
   const dataCount = document.getElementById("data-count");
   const newsContainer = document.getElementById("news-container");
-  if (data.length === 0) {
+  if (news.length === 0) {
     Swal.fire({
       icon: "error",
       title: "No Data Found",
@@ -52,96 +51,93 @@ const displayNews = (data, name) => {
   } else {
     dataCount.classList.remove("d-none");
     dataCount.innerHTML = `
-    <h3 class="text-center">
-          <span class="text-info">${name}</span> contain <span class="text-info">${data.length}</span> items
-        </h3>
+    <h4 class="text-center fw-bold">
+          <span style='color: #5d5fef'>${name}</span> contain <span style='color: #5d5fef'>${news.length}</span> items
+        </h4>
     `;
   }
 
-  newsContainer.innerHTML = "";
   sortedByView.forEach((news) => {
     const div = document.createElement("div");
-    div.classList.add("card-shadow");
+    div.classList.add("card-shadow", "mb-5");
     div.innerHTML = `
-      <div class="card-shadow">
-          <div class="mb-3 p-3">
-            <div
-              class="row g-0 d-flex justify-content-center align-items-center"
-            >
-              <div class="col-md-3 col-12 text-center text-md-start">
-                <img
-                  src="${news.thumbnail_url}"
-                  class="img-fluid"
-                  alt="..."
-                />
+        <div
+            class="row d-flex justify-content-between align-items-center p-3"
+          >
+            <div class="col-md-3 col-12 text-center text-md-start">
+              <img
+                src="${news.thumbnail_url}"
+                alt=""
+                class="img-fluid"
+              />
+            </div>
+            <div class="col-md-9 col-12 my-3 my-md-0">
+              <div>
+                <h4 class="fw-semibold">${news.title}</h4>
+                <p>${news.details.slice(0, 300)}...</p>
               </div>
-              <div class="col-md-9 col-12 my-2">
-                <div class="card-body">
-                  <h5 class="card-title">${news.title}</h5>
-                  <p class="card-text">${news.details.slice(0, 300)}...</p>
+              <div
+                class="row d-flex justify-content-between align-items-center"
+              >
+                <div class="col-12 col-md-3 mb-2">
+                  <img
+                    src="${
+                      news.author.img
+                        ? news.author.img
+                        : '<span class="text-danger fw-bold">Author Image Not Found</span>'
+                    }"
+                    alt=""
+                    class="img-fluid rounded-circle"
+                    style="width: 40px; height: 40px"
+                  />
+                  <span class="d-inline-block fw-bold text-secondary"
+                    >${
+                      news.author.name
+                        ? news.author.name
+                        : '<span class="text-danger fw-bold">Author Name Not Found</span>'
+                    }</span
+                  >
+                  <span class="d-inline-block text-secondary">
+                  
+                 ${
+                   news.author.published_date
+                     ? news.author.published_date
+                     : '<span class="text-danger fw-bold">Published Date Not Found</span>'
+                 }</span
+                  
+                  </span>
                 </div>
-                <div
-                  class="row d-flex justify-content-between align-items-center mt-2"
-                >
-                  <div class="col-md-4 col-6">
-                    <div
-                      class="row d-flex justify-content-center align-items-center"
-                    >
-                      <div class="col-md-3 col-5">
-                        <img
-                          src="${news.author.img}"
-                          alt=""
-                          class="img-fluid rounded-circle"
-                        />
-                      </div>
-                      <div class="col-md-9 col-7">
-                        <p class="fw-bold text-secondary">${
-                          news.author.name
-                            ? news.author.name
-                            : "<span class='text-danger'>Author name not found</span>"
-                        }</p>
-                        <p>${
-                          news.author.published_date
-                            ? news.author.published_date
-                            : "<span class='text-danger'>Published date not found</span>"
-                        }</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="col-md-2 col-6">
-                    <i class="fa-regular fa-eye"></i>
-                    <span class="text-secondary fw-bold">${
-                      news.total_view
-                        ? news.total_view
-                        : "<span class='text-danger'>0 Views</span>"
-                    }</span>
-                  </div>
-                  <div class="col-md-3 col-6">
-                    <i class="fa-solid fa-star-half-stroke"></i>
-                    <i class="fa-regular fa-star"></i>
-                    <i class="fa-regular fa-star"></i>
-                    <i class="fa-regular fa-star"></i>
-                    <i class="fa-regular fa-star"></i>
-                  </div>
-                  <div class="col-md-3 col-6">
-                    <button
+                <div class="col-md-3 col-6">
+                  <i class="fa-regular fa-eye"></i>
+                  ${
+                    news.total_view
+                      ? news.total_view
+                      : '<span class="text-danger fw-bold">0 Viewer</span>'
+                  }
+                </div>
+                <div class="col-md-3 d-none d-md-inline-block">
+                  <i class="fa-regular fa-star-half-stroke"></i>
+                  <i class="fa-regular fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                </div>
+                <div class="col-md-3 col-6">
+                  <button
                       type="button"
-                      class="btn btn-outline-info"
+                      class="btn-2 border-0 fw-bold"
                       data-bs-toggle="modal"
                       data-bs-target="#exampleModal"
                       onclick="loadDetails('${news._id}')"
                     >
                       See details
                     </button>
-                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      `;
+    `;
     newsContainer.appendChild(div);
-    toggleLoader(false);
   });
 };
 
@@ -158,63 +154,62 @@ const loadDetails = async (id) => {
 };
 
 const displayDetails = (details) => {
-  const modalBody = document.getElementById("m-body");
-  modalBody.innerHTML = ``;
   const modalTitle = document.getElementById("exampleModalLabel");
   modalTitle.innerText = details.title;
-  const div = document.createElement("div");
-  div.classList.add("card", "h-100");
-  div.innerHTML = `
-        <img src="${details.image_url}" class="card-img-top" alt="...">
-        <div class="card-body">
-            <p class="card-text">${details.details}</p>
-             <div
-                  class="row d-flex justify-content-between align-items-center mt-2"
-                >
-                  <div class="col-6">
-                    <div
-                      class="row d-flex justify-content-center align-items-center"
-                    >
-                      <div class="col-6">
-                        <img
-                          src="${details.author.img}"
-                          alt=""
-                          class="img-fluid rounded-circle"
-                        />
-                      </div>
-                      <div class="col-12">
-                        <p class="fw-bold text-secondary">${
-                          details.author.name
-                            ? details.author.name
-                            : "<span class='text-danger'>Author name not found</span>"
-                        }</p>
-                        <p>${
-                          details.author.published_date
-                            ? details.author.published_date
-                            : "<span class='text-danger'>Published date not found</span>"
-                        }</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="col-6">
-                    <i class="fa-regular fa-eye"></i>
-                    <span class="text-secondary fw-bold">${
-                      details.total_view
-                        ? details.total_view
-                        : "<span class='text-danger'>0 Views</span>"
-                    }</span>
-                  </div>
-                  <div class="col-6">
-                    <i class="fa-solid fa-star-half-stroke"></i>
-                    <i class="fa-regular fa-star"></i>
-                    <i class="fa-regular fa-star"></i>
-                    <i class="fa-regular fa-star"></i>
-                    <i class="fa-regular fa-star"></i>
-                  </div>
+  const modalBody = document.getElementById("m-body");
+  modalBody.innerHTML = `
+  <div class="card h-100">
+      <img src="${details.image_url}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <p class="card-text">${details.details}</p>
+      </div>
+      <div class="row d-flex justify-content-between align-items-center">
+                <div class="col-12 mb-2">
+                  <img
+                    src="${
+                      details.author.img
+                        ? details.author.img
+                        : '<span class="text-danger fw-bold">Author Image Not Found</span>'
+                    }"
+                    alt=""
+                    class="img-fluid rounded-circle"
+                    style="width: 40px; height: 40px"
+                  />
+                  <span class="d-inline-block fw-bold text-secondary"
+                    >${
+                      details.author.name
+                        ? details.author.name
+                        : '<span class="text-danger fw-bold">Author Name Not Found</span>'
+                    }</span
+                  >
+                  <span class="d-inline-block text-secondary">
+                  
+                 ${
+                   details.author.published_date
+                     ? details.author.published_date
+                     : '<span class="text-danger fw-bold">Published Date Not Found</span>'
+                 }</span
+                  
+                  </span>
                 </div>
-        </div>
-    `;
-  modalBody.appendChild(div);
+                <div class="col-6">
+                  <i class="fa-regular fa-eye"></i>
+                  ${
+                    details.total_view
+                      ? details.total_view
+                      : '<span class="text-danger fw-bold">0 Viewer</span>'
+                  }
+                </div>
+                <div class="col-6">
+                  <i class="fa-regular fa-star-half-stroke"></i>
+                  <i class="fa-regular fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                </div>
+              </div>
+    </div>
+  `;
 };
 
 const toggleLoader = (isLoading) => {
